2376e675d51b9ec79e70cbb6d3c55cae4522a3f0
Emily Conibear <63957778+econibear@users.noreply.github.com>
2021-10-11 14:25:28 +0100

    Update count_matrix.R

--- count_matrix.R
+++ count_matrix.R
@@ -1,17 +1,24 @@
+#Set working directory
 setwd("/rds/general/project/ins_rna_seq/live/")
 
+#Load in required packages
 library(DESeq2)
 library(RColorBrewer)
 library(gplots)
 library(Rsubread)
 
+#Load in file with metadata in
 aligned = read.csv("aligned_sample_names_v2.csv")
+
+#For some reason it makes files a factor, so convert this to character for feature Counts
 files <- aligned$final_name
 files1 = as.character(files)
 
-
+#featureCounts run
 fc<-featureCounts(files=files1,
                   annot.ext="/rds/general/project/ins_rna_seq/live/genomes/unindexed/GCF_000001405.39_GRCh38.p13_genomic.gtf",
                   isGTFAnnotationFile=TRUE,
                   isPairedEnd=TRUE)
+
+#Save output as a text file
 write.table(fc,"featureCounts_matrix.txt")
